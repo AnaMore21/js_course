2fc4890e2468046a1768014783fe2b7b8f145764
AnaMore21 <anastasiakrut1@gmail.com>
2021-11-23 17:19:18 +0200

    feat: implement loops and conditional last films, add result in object

--- js/script.js
+++ js/script.js
@@ -8,20 +8,48 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-console.log(personalMovieDB);
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?');
-let feedback = prompt('На сколько оцените его?');
+for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt('1 Один из последних просмотренных фильмов?');
+    let feedback = prompt('1 На сколько оцените его?');
 
-let lastFilm2 = prompt('Один из последних просмотренных фильмов?');
-let feedback2 = prompt('На сколько оцените его?');
+    if (lastFilm != '' && lastFilm != null && lastFilm.length < 50 &&
+        feedback !== '' && feedback !== null && feedback.length < 50) {
+        personalMovieDB.movies[lastFilm] = feedback;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-let movies = {
-    'lastFilm': feedback,
-    'lastFilm2': feedback2,
-};
+// let i = 0;
+// while (i < 2) {
+//     let lastFilm = prompt('2 Один из последних просмотренных фильмов?');
+//     let feedback = prompt('2 На сколько оцените его?');
+//     personalMovieDB.movies[lastFilm] = feedback;
+//     i++;
+//     console.log('done');
+// }
 
-personalMovieDB.movies[lastFilm] = feedback;
-personalMovieDB.movies[lastFilm2] = feedback2;
+// let i = 0;
+// do {
+//     let lastFilm = prompt('3 Один из последних просмотренных фильмов?');
+//     let feedback = prompt('3 На сколько оцените его?');
+//     personalMovieDB.movies[lastFilm] = feedback;
+//     i++;
+// } while (i < 2); {
+//     console.log('done');
+// }
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
