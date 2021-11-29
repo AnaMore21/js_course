5b50b1e78e08291278deaa009681824119dad20a
AnaMore21 <anastasiakrut1@gmail.com>
2021-11-29 14:45:00 +0200

    feat:use function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '15');
-console.log(numberOfFilms);
+"use strict";
+
+let numberOfFilms;
+
+function start(numberOfFilms) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '15');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '15');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,47 +20,36 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let lastFilm = prompt('1 Один из последних просмотренных фильмов?');
-    let feedback = prompt('1 На сколько оцените его?');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let lastFilm = prompt('1 Один из последних просмотренных фильмов?');
+        let feedback = prompt('1 На сколько оцените его?');
 
-    if (lastFilm != '' && lastFilm != null && lastFilm.length < 50 &&
-        feedback !== '' && feedback !== null && feedback.length < 50) {
-        personalMovieDB.movies[lastFilm] = feedback;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+        if (lastFilm != '' && lastFilm != null && lastFilm.length < 50 &&
+            feedback !== '' && feedback !== null && feedback.length < 50) {
+            personalMovieDB.movies[lastFilm] = feedback;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-// let i = 0;
-// while (i < 2) {
-//     let lastFilm = prompt('2 Один из последних просмотренных фильмов?');
-//     let feedback = prompt('2 На сколько оцените его?');
-//     personalMovieDB.movies[lastFilm] = feedback;
-//     i++;
-//     console.log('done');
-// }
+rememberMyFilms();
 
-// let i = 0;
-// do {
-//     let lastFilm = prompt('3 Один из последних просмотренных фильмов?');
-//     let feedback = prompt('3 На сколько оцените его?');
-//     personalMovieDB.movies[lastFilm] = feedback;
-//     i++;
-// } while (i < 2); {
-//     console.log('done');
-// }
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотренно довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотренно довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
